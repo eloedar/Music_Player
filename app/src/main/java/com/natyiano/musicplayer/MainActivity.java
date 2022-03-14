4b01129bd65524cd74a86fe22ec9f2cd6e052f07
eloedar <jiaji5@live.com>
2022-03-14 15:57:56 +0800

    Default Changelist

--- app/src/main/java/com/natyiano/musicplayer/MainActivity.java
+++ app/src/main/java/com/natyiano/musicplayer/MainActivity.java
@@ -2,10 +2,29 @@ package com.natyiano.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.view.View;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    public ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +33,45 @@ public class MainActivity extends AppCompatActivity {
 
         init();
     }
+
     public void init(){
-        
-        Intent intent = new Intent(MainActivity.this,MusicService.class);
+        //指定MainActivity与MusicService之间要连接；
+        Intent mintent = new Intent(MainActivity.this,MusicService.class);
+        bindService(mintent,connection,BIND_AUTO_CREATE);
 
     }
+
+    class OnClick implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn_play:{}
+                break;
+                case R.id.btn_pause:{}
+                break;
+            }
+        }
+    }
+    class seekBarlistener implements SeekBar.OnSeekBarChangeListener{
+        @Override
+        //进度条行进时候的监听
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+        }
+
+        @Override
+        //点击进度条开始拖动进度条的监听
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        //停止拖动时候的监听
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+
+    public static Handler mhandler = new Handler(Looper.myLooper()){};
 
 }
--- app/src/main/java/com/natyiano/musicplayer/MusicService.java
+++ app/src/main/java/com/natyiano/musicplayer/MusicService.java
@@ -2,6 +2,7 @@ package com.natyiano.musicplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class MusicService extends Service {
@@ -14,4 +15,23 @@ public class MusicService extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+    class MusicControl extends Binder {
+
+    }
+
+    public void addTimer(){}
 }
